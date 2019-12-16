391d55afa4f5bf5ad8c374717c225be230096272
GuillemArman <guillemarman8@gmail.com>
2019-12-16 14:35:01 +0100

    Still working on Matchmaking

--- EndlessRunnerMultiplayer/Assets/Scripts/CustomNetworkManager.cs
+++ EndlessRunnerMultiplayer/Assets/Scripts/CustomNetworkManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
+using UnityEngine.UI;
 
 
 public class MsgTypes
@@ -15,13 +18,43 @@ public class MsgTypes
 }
 public class CustomNetworkManager : NetworkManager
 {
+   
+
+    private RectTransform listOfMatches;
+    private GameObject matchEntryPrefab;
+    private NetworkID networkId;
+
+    public Text newMatchName;
+
     public short playerPrefabIndex;
+
+    public string[] playerNames = new string[] { "Boy", "Girl", "Robot" };
+
+    float time = 9999.0f;
+    
+
     public override void OnStartServer()
     {
         NetworkServer.RegisterHandler(MsgTypes.PlayerPrefabSelect, OnPrefabResponse);
         base.OnStartServer();
 
     }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 5.0f)
+        {
+            time = 0.0f;
+
+            NetworkManager.singleton.StartMatchMaker();
+            NetworkManager.singleton.matchMaker.ListMatches(0, 10, "", true, 0, 0, OnMatchList);
+
+
+        }
+    }
+
     public override void OnClientConnect(NetworkConnection conn)
     {
         client.RegisterHandler(MsgTypes.PlayerPrefabSelect, OnPrefabRequest);
@@ -48,7 +81,7 @@ public class CustomNetworkManager : NetworkManager
         base.OnServerAddPlayer(netMsg.conn, msg.controllerId);
     }
 
-    public string[] playerNames = new string[] { "Boy", "Girl", "Robot" };
+  
 
     private void OnGUI()
     {
@@ -86,6 +119,88 @@ public class CustomNetworkManager : NetworkManager
         {
             GameObject obstacle = Instantiate(spawnPrefabs[id], (enemyPlayer.gameObject.transform.position + (Vector3.forward * 15)), Quaternion.identity);
             NetworkServer.Spawn(obstacle);
+        }
+    }
+
+    public void OnCreateMatchClicked()
+    {
+        Debug.Log("OnCreateMatchClicked" + newMatchName.text);
+        NetworkManager.singleton.StartMatchMaker();
+        NetworkManager.singleton.matchMaker.CreateMatch(newMatchName.text, 4, true, "", "", "", 0, 0, OnMatchCreate);
+    }
+
+    public void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        NetworkManager.singleton.StopMatchMaker();
+
+        if (success)
+        {
+            NetworkManager.singleton.StartHost(matchInfo);
+
+        }
+        else
+        {
+            Debug.Log("OnMatchCreate failed");
+        }
+
+
+    }
+  
+    public void OnJoinMatchClicked(NetworkID networkId)
+    {
+        singleton.StartMatchMaker();
+        singleton.matchMaker.JoinMatch(networkId, "", "", "", 0, 0, OnMatchJoin);
+
+    }
+
+    public void OnMatchJoin(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        NetworkManager.singleton.StopMatchMaker();
+
+        if (success)
+        {
+            NetworkManager.singleton.StartClient(matchInfo);
+        }
+        else
+        {
+            Debug.Log("OnMatchJoin failed");
+        }
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+
+        if (success)
+        {
+            // Destroy previous list
+            int childCount = listOfMatches.transform.childCount;
+            for (int i = 0; i < childCount; ++i)
+            {
+                Destroy(listOfMatches.transform.GetChild(i).gameObject);
+            }
+
+            //Insert new list of matches
+
+            for (int i = 0; i < matches.Count; ++i)
+            {
+                MatchInfoSnapshot match = matches[i];
+                string matchName = match.name;
+                UnityEngine.Networking.Types.NetworkID networkID = match.networkId;
+
+                GameObject gameObject = Instantiate(matchEntryPrefab, listOfMatches);
+                RectTransform rect = gameObject.GetComponent<RectTransform>();
+
+                rect.position = new Vector2(10, listOfMatches.position.y - (float)i * 50.0f);
+
+                Text text = gameObject.GetComponentInChildren<Text>();
+                text.text = "Match: " + matchName;
+                Button button = gameObject.GetComponentInChildren<Button>();
+                button.onClick.AddListener(delegate { OnJoinMatchClicked(networkId); });
+            }
+        }
+        else
+        {
+            Debug.Log("OnMatchList failed");
         }
     }
 }
